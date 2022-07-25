5f9f6adbe063eec6d13c6d9d00d5a682a1cd3b54
Jala Unterreiner <jala.binbek@outlook.com>
2022-07-25 09:01:46 -0400

    created react app, imported compomets to app.js

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
+import './App.css';
+
+
