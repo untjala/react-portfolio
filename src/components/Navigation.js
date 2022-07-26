7b32bf6cf9cb33143394bdfb75c29418020bda86
Jala Unterreiner <jala.binbek@outlook.com>
2022-07-26 16:12:18 -0400

    tyled nav bar, moving pages into their own folders so they are pair ed with css

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import '../components/Nav.css'
 
-function Navigation ({setPage}) {
-  return (
-    <div>
-      <a href="#home" onClick={() => setPage('home')}> Home</a>
-      <a href="#about" onClick={() => setPage('about')}> About</a>
-      <a href="#contact" onClick={() => setPage('contact')}> Contact</a>
-      <a href="#resume" onClick={() => setPage('resume')}> Resume</a>
+function Navigation({ setPage }) {
+return (
+  <nav className="navbar navbar-nav-scroll d-flex">
+    <div className="container-fluid sticky-top justify-content-end">
+      <a className="nav-link mx-3" href="#home" onClick={() => setPage('home')}> Home</a>
+      <a className="nav-link mx-3" href="#about" onClick={() => setPage('about')}> About</a>
+      <a className="nav-link mx-3" href="#contact" onClick={() => setPage('contact')}> Contact</a>
+      <a className="nav-link mx-3" href="#resume" onClick={() => setPage('resume')}> Resume</a>
     </div>
-  )
+  </nav>
+)
 }
 
 export default Navigation
