7f75a72b21e7ac80f7300e6e4fb32da2d51d6651
Jala Unterreiner <jala.binbek@outlook.com>
2022-07-27 09:38:15 -0400

    fixed gh pages deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import Navigation from './components/Nav/index'
-import Home from './components/Header/index'
+import About from './components/Header/index'
 import Contact from './components/Contact/index'
 import Resume from './components/Resume/index'
 import Portfolio from './components/Portfolio/index';
@@ -9,11 +9,11 @@ import Footer from './components/Footer/index'
 export default function App ({ loggedIn, setLoggedIn }) {
   // Lets log our our loggedIn variable to see it change in real time
   console.log('App -> loggedIn', loggedIn);
-  const [page, setPage] = useState('home');
+  const [page, setPage] = useState('about');
  
   let CurrentPage; 
   
-   if (page === 'home') CurrentPage = Home;
+   if (page === 'about') CurrentPage = About;
    if (page === 'portfolio') CurrentPage = Portfolio;
    if (page === 'contact') CurrentPage = Contact;
    if (page === 'resume') CurrentPage = Resume;
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,9 +3,9 @@ import './style.css'
 
 function Navigation({ setPage }) {
 return (
-  <nav className="navbar navbar-nav-scroll d-flex">
+  <nav className="navbar collapse navbar-collapse d-flex">
     <div className="container-fluid fixed-top justify-content-end">
-      <a className="nav-link mx-3" href="#home" onClick={() => setPage('home')}> Home</a>
+      <a className="nav-link mx-3" href="#about" onClick={() => setPage('about')}> About</a>
       <a className="nav-link mx-3" href="#portfolio" onClick={() => setPage('portfolio')}> Portfolio</a>
       <a className="nav-link mx-3" href="#contact" onClick={() => setPage('contact')}> Contact</a>
       <a className="nav-link mx-3" href="#resume" onClick={() => setPage('resume')}> Resume</a>
