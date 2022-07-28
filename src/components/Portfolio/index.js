07c7d08745f9d3ac34220c390ed09f6bf6508ab9
Jala Unterreiner <jala.binbek@outlook.com>
2022-07-28 16:31:50 -0400

    fixed portfolio card heights

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -10,26 +10,26 @@ function Portfolio() {
         <h1> My Work </h1>
       </header>
       <section className='portfolio-cards card-column'>
-        <div className='food card-body col-sm-10 h-50 text-center'>
+        <div className='card-body col-sm-10 h-50 text-center'>
           <a
             target="_blank"
             rel="noreferrer"
             href='https://pacific-everglades-44013.herokuapp.com'>
           <img className="food card card-img" src={food} alt="8bit game"/>
           </a>
-          
           <a
             target="_blank"
             rel="noreferrer"
             href='https://github.com/untjala/food-run'>
           </a>
         </div>
+
         <div className='card-body col-sm-10 h-50 text-center'>
           <a
             target="_blank"
             rel="noreferrer"
             href='https://raydover.github.io/project-01/'>
-              <img className="card card-img" src={word} alt="gif finding site demo"/>
+              <img className="food card card-img" src={word} alt="gif finding site demo"/>
           </a>
           <a
             target="_blank"
@@ -37,12 +37,13 @@ function Portfolio() {
             href='https://github.com/raydover/project-01'>
           </a>
         </div>
-        <div className='card-body col-sm-10 h-50 text-center team'>
+
+        <div className="card-body col-sm-10 h-50 text-center team">
           <a
             target="_blank"
             rel="noreferrer"
             href='https://github.com/untjala/team-profile-generator'>
-              <img className="card card-img" src={team} alt="cards for team members"/>
+              <img className="food card card-img" src={team} alt="cards for team members"/>
           </a>
         </div>
       </section>
